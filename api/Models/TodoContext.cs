560f056ebaab7b5cf0b2fce3adf4fe89279bf0d7
daedrag <daoducduy0511@gmail.com>
2017-05-03 00:22:13 +0800

    first api commit

---
+++ api/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
---
+++ api/README.md
@@ -0,0 +1,3 @@
+Web API with .NET Core
+
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-vsc
